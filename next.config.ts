d722cf75e9853f23c20149d66d76375d04a3ecec
harshpandey <harshpandeyiitian04@gmail.com>
2026-04-14 00:12:16 +0530

    fixed bug

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["pdf-parse", "mammoth"],
-  },
+  serverExternalPackages: ["pdf-parse", "mammoth", "canvas", "jsdom"],
   images: {
     remotePatterns: [{ protocol: "https", hostname: "*.supabase.co" }],
-  },
-  // Silence pdf-parse warnings during build
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals ?? []), "canvas", "jsdom"];
-    }
-    return config;
   },
 };
 
